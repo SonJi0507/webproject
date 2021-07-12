7179a31546a2aeda34dd305d4c902b5e22677cd3
son <jhson0507@gmail.com>
2021-07-12 18:30:19 +0900

    pythonanywhere

--- calcapp/admin.py
+++ calcapp/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-from import_export.admin import ExportActionModelAdmin, ImportExportMixin, ImportMixin
 from calcapp.models import Attendences, Blog
 
-class AH_Admin(ImportExportMixin, admin.ModelAdmin):
-    pass
 
-admin.site.register(Attendences, AH_Admin)
+admin.site.register(Attendences)#, AH_Admin)
 admin.site.register(Blog)
