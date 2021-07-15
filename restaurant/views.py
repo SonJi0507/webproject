bd8b1b48e442d8b6c3d835d3bf10736c60a2a46d
son <jhson0507@gmail.com>
2021-07-15 19:27:36 +0900

    views

--- restaurant/views.py
+++ restaurant/views.py
@@ -53,10 +53,13 @@ def restaurant_search(request):
         #webdriver.DesiredCapabilities.CHROME['acceptSslCerts']=True
         options = webdriver.ChromeOptions()
         options.add_argument('--headless')
+        options.add_argument('window-size=1920x1080')
+        options.add_argument("disable-gpu")
         options.add_argument('--no-sandbox')
         options.add_argument("--single-process")
         options.add_argument("--disable-dev-shm-usage")
-        driver =  webdriver.Chrome(executable_path='static/chromedriver.exe', chrome_options=options) #
+
+        driver =  webdriver.Chrome(chrome_options=options) #executable_path='static/chromedriver.exe', 
         
         
         # temp = True
